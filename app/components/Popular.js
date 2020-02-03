bb9fb052b1816a9772d9c93615e5c3e6bb8068dd
Ignacio Alonso <ignacio.alley@gmail.com>
2020-02-03 11:08:48 -0600

    Add proptypes and css

---
+++ app/components/Popular.js
@@ -0,0 +1,57 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function LanguagesNav({ selected, onUpdateLanguage }) {
+  const languages = ["All", "Javascript", "Ruby", "CSS", "Python", "Java"];
+
+  return (
+    <ul className="flex-center">
+      {languages.map(language => (
+        <li key={language}>
+          <button
+            className="btn-clear nav-link"
+            style={language === selected ? { color: "rgb(187,46,31)" } : null}
+            onClick={() => onUpdateLanguage(language)}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+LanguagesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired
+};
+
+export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: "All"
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage: selectedLanguage
+    });
+  }
+  render() {
+    const { selectedLanguage } = this.state;
+
+    return (
+      <React.Fragment>
+        <LanguagesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
+      </React.Fragment>
+    );
+  }
+}
--- app/index.js
+++ app/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import Popular from "./components/Popular";
 // Component:
 // State: Has a state that it manages (data) and it manages it's own state
 // Lifecycle: fetch data ƒrom an api, etc
 // UI - What the UI is going to look like, redenr is the description of what the UI is going to look like
 class App extends React.Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div className="container">
+        <Popular />
+      </div>
+    );
   }
 }
 
