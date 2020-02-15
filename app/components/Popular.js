445d9f666ee1de2fed31c8f1f9bdb363392dfe1a
Ignacio Alonso <ignacio.alley@gmail.com>
2020-02-15 12:23:32 -0600

    Follow up

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,8 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { fetchPopularRepos } from "../utils/api";
+import {
+  FaUser,
+  FaStar,
+  FaCodeBranch,
+  FaExclamationTriangle
+} from "react-icons/fa";
 
-function LanguagesNav({ selected, onUpdateLanguage }) {
-  const languages = ["All", "Javascript", "Ruby", "CSS", "Python", "Java"];
+function LangaugesNav({ selected, onUpdateLanguage }) {
+  const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
 
   return (
     <ul className="flex-center">
@@ -10,7 +17,7 @@ function LanguagesNav({ selected, onUpdateLanguage }) {
         <li key={language}>
           <button
             className="btn-clear nav-link"
-            style={language === selected ? { color: "rgb(187,46,31)" } : null}
+            style={language === selected ? { color: "rgb(187, 46, 31)" } : null}
             onClick={() => onUpdateLanguage(language)}
           >
             {language}
@@ -21,9 +28,65 @@ function LanguagesNav({ selected, onUpdateLanguage }) {
   );
 }
 
-LanguagesNav.propTypes = {
+LangaugesNav.propTypes = {
   selected: PropTypes.string.isRequired,
   onUpdateLanguage: PropTypes.func.isRequired
+};
+
+function ReposGrid({ repos }) {
+  return (
+    <ul className="grid space-around">
+      {repos.map((repo, index) => {
+        const {
+          name,
+          owner,
+          html_url,
+          stargazers_count,
+          forks,
+          open_issues
+        } = repo;
+        const { login, avatar_url } = owner;
+
+        return (
+          <li key={html_url} className="repo bg-light">
+            <h4 className="header-lg center-text">#{index + 1}</h4>
+            <img
+              className="avatar"
+              src={avatar_url}
+              alt={`Avatar for ${login}`}
+            />
+            <h2 className="center-text">
+              <a className="link" href={html_url}>
+                {login}
+              </a>
+            </h2>
+            <ul className="card-list">
+              <li>
+                <FaUser color="rgb(255, 191, 116)" size={22} />
+                <a href={`https://github.com/${login}`}>{login}</a>
+              </li>
+              <li>
+                <FaStar color="rgb(255, 215, 0)" size={22} />
+                {stargazers_count.toLocaleString()} stars
+              </li>
+              <li>
+                <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
+                {forks.toLocaleString()} forks
+              </li>
+              <li>
+                <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
+                {open_issues.toLocaleString()} open
+              </li>
+            </ul>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+ReposGrid.propTypes = {
+  repos: PropTypes.array.isRequired
 };
 
 export default class Popular extends React.Component {
@@ -31,26 +94,64 @@ export default class Popular extends React.Component {
     super(props);
 
     this.state = {
-      selectedLanguage: "All"
+      selectedLanguage: "All",
+      repos: {},
+      error: null
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
+    this.isLoading = this.isLoading.bind(this);
   }
-
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
   updateLanguage(selectedLanguage) {
     this.setState({
-      selectedLanguage: selectedLanguage
+      selectedLanguage,
+      error: null
     });
+
+    if (!this.state.repos[selectedLanguage]) {
+      fetchPopularRepos(selectedLanguage)
+        .then(data => {
+          this.setState(({ repos }) => ({
+            repos: {
+              ...repos,
+              [selectedLanguage]: data
+            }
+          }));
+        })
+        .catch(() => {
+          console.warn("Error fetching repos: ", error);
+
+          this.setState({
+            error: `There was an error fetching the repositories.`
+          });
+        });
+    }
+  }
+  isLoading() {
+    const { selectedLanguage, repos, error } = this.state;
+
+    return !repos[selectedLanguage] && error === null;
   }
   render() {
-    const { selectedLanguage } = this.state;
+    const { selectedLanguage, repos, error } = this.state;
 
     return (
       <React.Fragment>
-        <LanguagesNav
+        <LangaugesNav
           selected={selectedLanguage}
           onUpdateLanguage={this.updateLanguage}
         />
+
+        {this.isLoading() && <p>LOADING</p>}
+
+        {error && <p>{error}</p>}
+
+        {repos[selectedLanguage] && (
+          <ReposGrid repos={repos[selectedLanguage]} />
+        )}
       </React.Fragment>
     );
   }
---
+++ app/utils/api.js
@@ -0,0 +1,14 @@
+export function fetchPopularRepos(language) {
+  const endpoint = window.encodeURI(
+    `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=star&order=desc&type=Repositories`
+  );
+  return fetch(endpoint)
+    .then(res => res.json())
+    .then(data => {
+      if (!data.items) {
+        throw new Error(data.message);
+      }
+
+      return data.items;
+    });
+}
